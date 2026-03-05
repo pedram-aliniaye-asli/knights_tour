77a65a295218a237d0b9bc58d7bf4d238cdfde6d
pedram-aliniaye-asli <pedram.aliniayeasli@gmail.com>
2026-03-05 00:41:00 -0500

    built knight class and move method

--- knight_tour/main.py
+++ knight_tour/main.py
@@ -6,3 +6,20 @@ def make_board(width:int , height: int ) -> Dict:
 
 
 print(make_board(5,5))
+
+
+class knight:
+    def __init__(self):
+        pass
+    def move(self, board:List, x:int, y:int):
+        if (x,y) in board.keys():
+            if not board[(x,y)]:
+                board[(x, y)] = 1
+        else:
+            print('Moved out of the board!')
+        return board
+
+board= make_board(5,5)
+k = knight()
+print(k.move(board, 0, 2))
+print(k.move(board, 1, 3))
